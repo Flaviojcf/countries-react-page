4c3768e8a1e8f2f98d5ba21346b242f58e743ad8
Flavio <fjcf@poli.br>
2022-10-18 01:25:52 -0300

    created loading fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import { useState } from "react";
+import { QueryClientProvider } from "react-query";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { ThemeValueContextProvider } from "./contexts/ThemeValueContext";
 import { useTheme } from "./hooks/useTheme";
 import { Router } from "./Router";
+import { queryClient } from "./services/QueryClient";
 import { GlobalStyle } from "./styles/global";
 import { DarkTheme, LightTheme } from "./styles/themes/default";
 
 function App() {
   return (
     <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
       <ThemeValueContextProvider>
         <Router />
         <GlobalStyle />
       </ThemeValueContextProvider>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 }
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,5 +1,7 @@
-import { useEffect, useState } from "react";
+import React, { useState } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
 import { api } from "../../services/api";
+import { useQuery } from "react-query";
 import {
   Container,
   ContainerCountry,
@@ -9,34 +11,46 @@ import {
 
 export function Home() {
   const [country, setCountry] = useState<any[]>([]);
+  const { isFetching } = useQuery(["all"], () => getCountries(), {
+    staleTime: 1000 * 60 * 10,
+  });
 
   async function getCountries() {
     const response = await api.get("all");
     setCountry(response.data);
   }
 
-  useEffect(() => {
-    getCountries();
-  }, []);
-
   return (
     <Container>
       <ContainerGroupCountry>
-        {country.map((teste: any, number = 1) => (
-          <ContainerCountry key={teste.name.official}>
-            <img src={teste.flags.png} alt="" />
-            <ContainerCountryText>
-              <p>
-                {number + 1} {teste.name.official}
-              </p>
-              <span>
-                <h4>Population: {teste.population}</h4>
-                <h4>Region: {teste.region}</h4>
-                <h4>Capital: {teste.capital}</h4>
-              </span>
-            </ContainerCountryText>
-          </ContainerCountry>
-        ))}
+        {isFetching ? (
+          <ClipLoader
+            size={150}
+            color={"#123abc"}
+            loading={isFetching}
+            speedMultiplier={1.5}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        ) : (
+          <>
+            {country.map((teste: any, number = 1) => (
+              <ContainerCountry key={teste.name.official}>
+                <img src={teste.flags.png} alt="" />
+                <ContainerCountryText>
+                  <p>
+                    {number + 1} {teste.name.official}
+                  </p>
+                  <span>
+                    <h4>Population: {teste.population}</h4>
+                    <h4>Region: {teste.region}</h4>
+                    <h4>Capital: {teste.capital}</h4>
+                  </span>
+                </ContainerCountryText>
+              </ContainerCountry>
+            ))}
+          </>
+        )}
       </ContainerGroupCountry>
     </Container>
   );
